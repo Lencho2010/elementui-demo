190ea58f761406534bdb2ca51d63ff7bf30f2ccc
Lencho <335999737@qq.com>
2021-09-16 12:40:54 +0800

    质检详情和不通过图斑代码更新

--- src/api/contrast/checkInfo.js
+++ src/api/contrast/checkInfo.js
@@ -5,6 +5,16 @@ import ca from "element-ui/src/locale/lang/ca";
 /*export function listCheckResultTotal(taskName) {
   let url = `/checkResultTotal/listByCode/${taskName}`;
   return request.get(url);
+}
+
+export function listCheckDetail(taskName) {
+  let url = `/checkResult/listByCode/${taskName}`;
+  return request.get(url);
+}
+
+export function listCheckFail(taskName) {
+  let url = `/checkDetail/listByCode/${taskName}`;
+  return request.get(url);
 }*/
 
 // 获取质检汇总信息
@@ -15,13 +25,13 @@ export function listCheckResultTotal(taskName) {
 
 // 获取质检详情信息
 export function listCheckDetail(taskName) {
-  let url = `/checkResult/listByCode/${taskName}`;
+  let url = `/jctbQualityCheckDetail/list/${taskName}`;
   return request.get(url);
 }
 
 // 获取质检不通过信息
 export function listCheckFail(taskName) {
-  let url = `/checkDetail/listByCode/${taskName}`;
+  let url = `/jctbQualityCheckFail/list/${taskName}`;
   return request.get(url);
 }
 
